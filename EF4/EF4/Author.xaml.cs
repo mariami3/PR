1ee56b54e09f0d52e17456b676520678e1ed628b
mariami3 <144897983+mariami3@users.noreply.github.com>
2024-03-20 23:42:43 +0300

    Add files via upload

---
+++ EF4/EF4/Author.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EF4
+{
+    /// <summary>
+    /// Логика взаимодействия для Author.xaml
+    /// </summary>
+    public partial class Author : Window
+    {
+        private BookEntities context = new BookEntities();
+        public Author()
+        {
+            InitializeComponent();
+            Au.ItemsSource = context.Authors.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Au.ItemsSource = context.Authors.ToList().Where(item => item.AuthorName.Contains(Search.Text));
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Au.ItemsSource = context.Authors.ToList();
+        }
+    }
+}
---
+++ EF4/EF4/Booke.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EF4
+{
+    /// <summary>
+    /// Логика взаимодействия для Booke.xaml
+    /// </summary>
+    public partial class Booke : Window
+    {
+        private BookEntities booke = new BookEntities();
+        public Booke()
+        {
+            InitializeComponent();
+            bk.ItemsSource = booke.BooKs.ToList();
+            Cb.ItemsSource = booke.Authors.ToList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            bk.ItemsSource = booke.BooKs.ToList().Where(item => item.BooksName.Contains(Search.Text));
+            bk.ItemsSource = booke.BooKs.ToList().Where(item => item.DateCriation.Contains(Search.Text));
+            bk.ItemsSource = booke.BooKs.ToList().Where(item => item.TypeLit.Contains(Search.Text));
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            bk.ItemsSource = booke.BooKs.ToList();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Cb_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (bk.ItemsSource != null)
+            {
+                var selected = Cb.SelectedItem as Authors;
+                bk.ItemsSource = booke.BooKs.ToList().Where(item => item.Authors == selected);
+
+            }
+        }
+    }
+}
